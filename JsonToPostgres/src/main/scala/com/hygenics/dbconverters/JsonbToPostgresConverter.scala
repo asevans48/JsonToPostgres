d2c61406bf80c006f711dff1330fb6922a43f612
aevans <Andrew.Evans@hygenicsdatallc.com>
2016-05-27 17:18:14 -0600

    Initial Commit

---
+++ JsonToPostgres/src/main/scala/com/hygenics/dbconverters/JsonbToPostgresConverter.scala
@@ -0,0 +1,34 @@
+package com.hygenics.dbconverters
+
+import com.hygenics.database.DatabaseHandler
+
+import scala.concurrent.{Future,Await}
+import scala.util.{Success,Failure}
+
+class JsonbToPostgresConverter {
+    
+    def getData(sl : String)={
+      
+    }
+    
+    def convert()={
+      
+    }
+    
+    def post()={
+      
+    }
+    
+    def run(args : Array[String])={
+      
+    }
+}
+
+
+object Driver{
+  
+  def main(args : Array[String])={
+    new JsonbToPostgresConverter().run(args)
+  }
+  
+}
